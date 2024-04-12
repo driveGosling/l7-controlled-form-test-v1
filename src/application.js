dc37ae39fe2f604b72aaf21a7c7b29b5ecceac4a
toDoListEnjoyer <toDoListEnjoyer@gmail.com>
2024-04-12 17:20:08 +0300

    accidentally passed step 4

--- src/application.js
+++ src/application.js
@@ -1,4 +1,86 @@
 import axios from 'axios';
+import onChange from 'on-change';
+
+const isValidName = (name) => {
+  return /[\S]+/.test(name);
+};
+
+const isValidEmail = (email) => {
+  return /[\S]+@[\S]+/.test(email);
+};
+
+// MODEL START
+const formState = {
+  validationStates: { // так удобнее указать объект для просмотра onChange'ем
+    inputName: 'empty',
+    inputEmail: 'empty',
+  },
+
+  inputName: {
+    value: '',
+    isValidInput: isValidName,
+    validationState: 'empty',
+  },
+
+  inputEmail: {
+    value: '',
+    isValidInput: isValidEmail,
+    validationState: 'empty',
+  },
+};
+// MODEL END
+
+// VIEW START
+const toggleSwitch = () => {
+  const switchBtn = document.querySelector('input[type="submit"]');
+
+  if (formState.validationStates.inputName === 'valid' && formState.validationStates.inputEmail === 'valid') {
+    console.log('yes')
+    switchBtn.disabled = false;
+  } else {
+    console.log('no')
+    switchBtn.disabled = true;
+  }
+};
+
+const changeInputStyle = (inputId) => {
+  const validationState = formState[inputId].validationState;
+  const input = document.querySelector(`#${inputId}`);
+
+  input.classList.remove('form-control'); // багулина в тестах паходу
+  if (validationState === 'valid') {
+    input.classList.remove('is-invalid');
+    input.classList.add('is-valid');
+  } else if (validationState === 'invalid') {
+    input.classList.remove('is-valid');
+    input.classList.add('is-invalid');
+  } else {
+    input.classList.remove('is-valid');
+    input.classList.remove('is-invalid');
+  }
+  input.classList.add('form-control'); // багулина в тестах паходу
+
+  toggleSwitch();
+};
+// VIEW END
+
+const watchedValidation = onChange(formState.validationStates, (inputId, value) => {
+  formState[inputId].validationState = value; // строчка чтобы синхронизировать значения validationStates и [inputId].validationState
+  changeInputStyle(inputId);
+});
+
+const validate = (inputId) => {
+  const value = formState[inputId].value;
+  const isValidInput = formState[inputId].isValidInput;
+
+  if (isValidInput(value)) {
+    console.log('valid')
+    watchedValidation[inputId] = 'valid';
+  } else {
+    console.log('invalid')
+    watchedValidation[inputId] = 'invalid';
+  }
+};
 
 const formHtml = `
 <form id="registrationForm">
@@ -19,6 +101,8 @@ const form = () => {
   const formContainer = document.querySelector('.form-container');
   formContainer.innerHTML = formHtml;
 
+  toggleSwitch();
+
   // Step 2
   const submitBtn = document.querySelector('input[type="submit"]');
   submitBtn.addEventListener('click', (e) => {
@@ -27,6 +111,16 @@ const form = () => {
       .then((res) => {
         document.body.innerHTML = `<p>${res.data.message}</p>`;
       });
+  });
+
+  // Step 3
+  const form = document.querySelector('#registrationForm');
+  form.addEventListener('input', (e) => {
+    const inputId = e.target.id;
+    const inputValue = e.target.value;
+
+    formState[inputId].value = inputValue;
+    validate(inputId);
   });
 };
 
